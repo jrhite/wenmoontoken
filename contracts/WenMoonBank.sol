43e98a52523c2dcd0d7b89b373f2b2b8e0320108
Jacob Hite <1085644+jrhite@users.noreply.github.com>
2022-06-01 08:52:05 +0700

    add deposit bonus logic

--- contracts/WenMoonBank.sol
+++ contracts/WenMoonBank.sol
@@ -7,8 +7,13 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 
 contract WenMoonBank {
-  // mapping: ERC20 token address => account address => uint256
-  mapping(address => mapping (address => uint256)) balances;
+  struct UserAccount {
+    bool isNotFirstDeposit;
+    uint256 balance;
+  }
+
+  // mapping: ERC20 token address => account address => UserAccount
+  mapping(address => mapping (address => UserAccount)) userAccounts;
 
   //
   // TODO: implement deposit function
@@ -17,23 +22,44 @@ contract WenMoonBank {
     //
     // can add extra deposit logic here, eg: add bonus multiplier to deposit
     //
-    
+    uint256 bonusPercentage = 5;
+
+    //
+    // Calculate deposit bonus
+    //
+    // Notes
+    //
+    //   * avoid doing fractional math in Solidity
+    //   * we have 18 decimals of precision, so we have a lot of room to work with
+    //   * mulitply then divide!
+    //       * mulitply bonus percent first, then divide by 100 (10 ** 2) to convert the
+    //         value into a percentage
+    //       * be careful multiplcation doesn't overflow (eg: number doesn't exceed 10 ** 36)
+    //   
+    uint256 bonus = (amount *  bonusPercentage) / 10**2;
+
     IERC20(erc20TokenAddress).transferFrom(msg.sender, address(this), amount);
 
-    balances[erc20TokenAddress][msg.sender] += amount;
+    userAccounts[erc20TokenAddress][msg.sender].balance += amount;
+
+    if (! userAccounts[erc20TokenAddress][msg.sender].isNotFirstDeposit) {
+      IERC20(erc20TokenAddress).transfer(msg.sender, bonus);
+    }
+
+    userAccounts[erc20TokenAddress][msg.sender].isNotFirstDeposit = true;
   }
 
   //
   // TODO: implement withdraw function
   //
   function withdraw(address erc20TokenAddress, uint256 amount) external {
-    require(balances[erc20TokenAddress][msg.sender] >= amount);
+    require(userAccounts[erc20TokenAddress][msg.sender].balance >= amount);
 
     //
     // can add extra withdraw logic here, eg: calculate interest
     //
 
-    balances[erc20TokenAddress][msg.sender] -= amount;
+    userAccounts[erc20TokenAddress][msg.sender].balance -= amount;
 
     IERC20(erc20TokenAddress).transfer(msg.sender, amount);
   }
